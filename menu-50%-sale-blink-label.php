a5add35ec1b4a44b06d293b6a8cbf728167beadd
Ibrahim Monir <54437935+ibrahim-monir@users.noreply.github.com>
2024-07-10 10:00:51 +0600

    First Commit

---
+++ menu-50%-sale-blink-label.php
@@ -0,0 +1,46 @@
+/* Main Menu */
+.deen_sale_btn::before,
+.elementor-element-cf9fad1:before{
+	content: "50%";
+	color: #fff;
+	font-size: 12px;
+	padding: 2px 5px;
+	border-radius: 15px;
+	right: 0;
+	top: -12px;
+	background: #FF6900;
+	position: absolute;
+	animation-name: deenimation;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+}
+
+@media only screen and (max-width:1024px){
+	.elementor-element-cf9fad1:before{
+	right: 150px;
+	top: -6px;
+		padding: 0 6px;
+}
+}
+
+@media only screen and (max-width:768px){
+	.elementor-element-cf9fad1:before{
+	right: 95px;
+	top: -4px;
+}
+}
+
+@media only screen and (max-width:425px){
+	.elementor-element-cf9fad1:before{
+	right: 4px;
+	top: 2px;
+		font-size:10px;
+}
+}
+
+@media only screen and (max-width:375px){
+	.elementor-element-cf9fad1:before{
+	right: -8px;
+	top: -6px;
+}
+}
